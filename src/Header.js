69d492a5a2afabeb30253ccd3564cb4f27a00d07
ELIJAHKUNGU <elijahkungu100@gmail>
2022-04-11 20:48:02 +0300

    tinder-clone-header

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './header.css'
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import ForumIcon from '@mui/icons-material/Forum';
+import IconButton from '@material-ui/core/IconButton';
+
+const Header = () => {
+  return (
+    <div className="header">
+      {/* BEM */}
+      <IconButton>
+        <PermIdentityIcon className="header_icon" fontSize="large"/>
+        </IconButton>
+        <img className="header__logo" src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-logo-700x394.png" alt="" srcset="" />
+        {/* <img src="https://tinder.com/static/tinder.png" alt="" srcset="" /> */}
+        <IconButton>
+        <ForumIcon className="header_icon" fontSize="large"/>
+        </IconButton>
+    </div>
+  )
+}
+
+export default Header
